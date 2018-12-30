6c66468b29a6d3947da0bbd7489cc2518404dd69
HeckerJay <43544546+HeckerJay@users.noreply.github.com>
2018-12-30 13:41:12 +0000

    Update 929team.py
    Change some syntax.

--- 929team.py
+++ 929team.py
@@ -6,44 +6,44 @@ import re
 #==============================================================================
 # 处理个人数据集
 
-jobcat={}
+indcat={}
 with open('resume_dataset.csv','r',encoding='UTF-8') as compaire:
     comp = csv.DictReader(compaire)
     column2 = [row['Category'] for row in comp]
-    for line2 in column2: ##比较新建的counts库和这个人有没有counts 库内的数据 没有推荐剩余
-        if line2 in jobcat:
-                jobcat[line2] += 1
+    for line2 in column2:           #比较新建的counts库和这个人有没有counts 库内的数据 没有推荐剩余
+        if line2 in indcat:
+                indcat[line2] += 1
         else:
-                jobcat[line2] = 1
+                indcat[line2] = 1
 
 
-a=filter(lambda x:x[1] in range(5), jobcat.items())
+a=filter(lambda x:x[1] in range(5), indcat.items())
 for key,value in list(a):
-    del jobcat[key] #删除掉低频率项
+    del indcat[key] #删除掉低频率项
 
 # 处理公司数据集，找到对应职业信息    
 
 #==============================================================================
 # 处理公司数据集
 with open('stoplist.txt','r',encoding='UTF-8') as f:
-    stops = f.read()#stoplist.自己新建一个stoplist.txt,每次运行一遍下面程序去找无用的关键语句
+    stops = f.read() #stoplist.自己新建一个stoplist.txt,每次运行一遍下面程序去找无用的关键语句
 
-# 处理个人数据集
 
-indcat={}
+
+jobcat={}
 with open('job_skills.csv','r',encoding='UTF-8') as compaire0:
     comp0 = csv.DictReader(compaire0)
     column1 = [row['Category'] for row in comp0]
     for line1 in column1: ##比较新建的counts库和这个人有没有counts 库内的数据 没有推荐剩余
-        if line2 in indcat:
-                indcat[line1] += 1
+        if line1 in jobcat:
+                jobcat[line1] += 1
         else:
-                indcat[line1] = 1
+                jobcat[line1] = 1
 
 
-b=filter(lambda x:x[1] in range(5), indcat.items())
+b=filter(lambda x:x[1] in range(5), jobcat.items())
 for key,value in list(b):
-    del indcat[key] #删除掉低频率项
+    del jobcat[key] #删除掉低频率项
 
 counts={}
 with open('job_skills.csv','r',encoding='UTF-8') as csvfile:
@@ -62,5 +62,10 @@ with open('job_skills.csv','r',encoding='UTF-8') as csvfile:
 pairs = sorted(counts, key=lambda v:counts[v], reverse=True)
 print(pairs[:10]) #找前十
 
+finalcat=[]
+for aimcat in  jobcat:
+    if aimcat in indcat:
+        finalcat.append(aimcat)
+        
 
         
