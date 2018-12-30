0e68c72af764cc921ae287dcb09ac31215ed32f4
HeckerJay <43544546+HeckerJay@users.noreply.github.com>
2018-12-30 17:40:08 +0000

    good

--- 929team.py
+++ 929team.py
@@ -1,11 +1,8 @@
 import csv
 import re
 
-
-
 #==============================================================================
-# treat individuals' dataset
-
+# treat individuals' dataset， find out corresponded categories
 indcat={}
 with open('resume_dataset.csv','r',encoding='UTF-8') as compaire:
     comp = csv.DictReader(compaire)
@@ -20,11 +17,9 @@ with open('resume_dataset.csv','r',encoding='UTF-8') as compaire:
 a=filter(lambda x:x[1] in range(5), indcat.items())
 for key,value in list(a):
     del indcat[key] #delete redundant information
-
-# treat companies' dataset，find out corresponded categories
-
+    
 #==============================================================================
-# 处理公司数据集
+# treat companies' dataset， find out corresponded categories
 jobcat={}
 with open('job_skills.csv','r',encoding='UTF-8') as compaire0:
     comp0 = csv.DictReader(compaire0)
@@ -44,7 +39,7 @@ for key,value in list(b):
 finalcat=[]
 for aimcat in  jobcat:
     if aimcat in indcat:
-        finalcat.append(aimcat)
+        finalcat.append(aimcat) #check the same categories dataset between two .csv
         
 #==============================================================================
 #比较新建的counts库和这个人有没有counts 库内的数据 没有推荐剩余 
@@ -58,22 +53,35 @@ with open('job_skills.csv','r',encoding='UTF-8') as csvfile:
     column = [row['Minimum Qualifications'] for row in reader]#选列名
     for line in column:
       if 'in ' in line:  
-        word = re.compile(r'.*, (.*).*,')#目前最优的正则化语句
+        word = re.compile(r'.*, (.*).*,') #RE compile
         line=line.replace(r'in ',r', ')
         line=line.replace(r'.',r', ')
+        line=line.replace(r')',r', ')
         line=line.replace('and ',r', ')
-        line=line.replace('or ',r', ')#替换
+        line=line.replace('or ',r', ') #replace symbol of keyword's position
         for wd in word.findall(line):
-          if wd not in stops:#use stoplist to avoid unexcepted info
+          if wd not in stops: #use stoplist to avoid unexcepted info
             if wd in counts:
                 counts[wd] += 1
             else:
                 counts[wd] = 1
 pairs = sorted(counts, key=lambda v:counts[v], reverse=True)
-print(pairs[:10]) #find out most 10 frequent skills
-        
+pairs = pairs[:15]#find out most 15 frequent skills
 
-
-        
-
-        
+comprow=[]
+with open('resume_dataset.csv','r',encoding='UTF-8') as csvfile:
+    comp = csv.DictReader(csvfile)
+    for row in comp:
+        if row['Category'] == 'Information Technology':
+            comprow.append(row['ID'])
+            
+recomdlesson={}
+with open('resume_dataset.csv','r',encoding='UTF-8') as csvfile:
+    comp = csv.DictReader(csvfile)
+    for row in comp:
+        if row['ID'] in comprow:
+            recomdlesson[row['ID']]='Recommended Lesson: '
+            for check in pairs:
+                recless=re.compile(r''+check+'')
+                if recless.findall(row['Resume'])==[]:
+                        recomdlesson[row['ID']]+=check+';'
